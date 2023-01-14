4b4db4d59a0b8bd77afd8d353fe4823dd4fdb8b9
Dubem Otekeiwebia <dondubbie007@gmail.com>
2023-01-14 22:14:06 +0100

    Add project files.

---
+++ IDServices/Program.cs
@@ -0,0 +1,24 @@
+using IDServices;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddTestUsers(IdentityConfiguration.TestUsers)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+
+app.Run();
